c15732036ceb90350e8f3287c434da926258693a
Flavio Silva <flavio1110@gmail.com>
2023-09-02 20:22:18 +0200

    publish stable image on tag

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -10,6 +10,12 @@ command -v docker >/dev/null 2>&1 || { echo 'please install docker'; exit 1; }
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ./bin/rinha-backend .
 
 image="flavio1110/rinha-backend"
-tag="local"
+tag="${tag:=local}"
 
-docker build --no-cache=true -t "${image}:${tag}" .
+if [ "$tag" = "stable" ]
+then
+  docker buildx create --use
+  docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --no-cache=true -t "${image}:${tag}" --push .
+else
+  docker build --no-cache=true -t "${image}:${tag}" .
+fi
